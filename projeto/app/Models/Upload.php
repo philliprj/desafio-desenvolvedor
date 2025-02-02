ccf71cbbe57a5f585fcd63289b101916b105041c
Thiago Barbosa <thiagophrj@gmail.com>
2025-02-01 23:34:18 -0300

    Adiciona models para upload e histórico

---
+++ projeto/app/Models/Upload.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Upload extends Model
+{
+    use HasFactory;
+
+    protected $table = 'uploads';
+
+    protected $fillable = [
+        'upload_history_id',
+        'RptDt',
+        'TckrSymb',
+        'MktNm',
+        'SctyCtgyNm',
+        'ISIN',
+        'CrpnNm',
+    ];
+
+    public function uploadHistory()
+    {
+        return $this->belongsTo(UploadHistory::class, 'upload_history_id', 'id');
+    }
+}
---
+++ projeto/app/Models/UploadHistory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UploadHistory extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'file_name',
+        'file_hash',
+        'reference_date',
+    ];
+
+    public function uploads()
+    {
+        return $this->hasMany(Upload::class, 'upload_history_id', 'id');
+    }
+}
